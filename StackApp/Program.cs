ba5d920a13a26d946f188b5d5c8f24ad5029f917
GingerBeardx <ecryptographer@gmail.com>
2020-11-02 18:32:05 -0700

    Add project files.

---
+++ StackApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stacker();
+
+            stack.Push(3);
+            stack.Push("Eric");
+            stack.Push(null);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackApp/Stacker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackApp
+{
+    class Stacker
+    {
+        private List<object> Stack { get; set; } = new List<object>();
+
+        public void Push(object item)
+        {
+            if (item == null)
+                throw new InvalidOperationException("Item cannot be null");
+
+            Stack.Add(item);
+            Console.WriteLine("Item added");
+        }
+    }
+}
